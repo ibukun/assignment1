bc3ec55e3d4b899746e8cf832fd5ff26e776b793
ibukun <blessingshey@yahoo.co.uk>
2012-11-05 22:19:19 +0000

    initial project version

---
+++ Documents/javaworkspace/Tools/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+		public static void main (String args[]){
+			double ib = 89; 
+			System.out.println("Hello World");
+			System.out.println(ib);
+		}
+
+	}
+
+
+
