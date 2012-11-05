3802fbb6b3a5bff638288b7a5c88c1be2963ddae
ibukun <blessingshey@yahoo.co.uk>
2012-11-05 23:32:11 +0000

    update of another java class

---
+++ Documents/javaworkspace/Tools/src/HelloWorld/AnotherClass.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package HelloWorld;
+
+/**
+ * @author ibukun
+ *
+ */
+public class AnotherClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my java class for GitHub");
+		System.out.println("Review against another vcs");
+		
+
+
+	}
+
+}
